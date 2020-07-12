741c3d5d0c3a411db0e75634966efd8459bc2607
Rishabh <grishabh1992@gmail.com>
2020-07-12 10:32:47 +0530

    Enabled CORS

--- app/app.ts
+++ app/app.ts
@@ -1,17 +1,18 @@
 import * as express from 'express'
 import { AppRoutes } from './routes';
 import bodyParser = require('body-parser');
+import * as cors from 'cors';
 
 class App {
   public express : express.Application;
 
   constructor () {
     this.express = express();
-    this.mountRoutes();
-    
+    this.mountRoutes();    
   }
 
   private mountRoutes (): void {
+    this.express.use(cors())
     this.express.use(bodyParser.json());
     this.express.use(new AppRoutes().routes);
   }
